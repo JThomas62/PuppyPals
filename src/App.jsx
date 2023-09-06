66e97ef5b9d0351ddd39063ac08f4381919cca98
JThomas62 <jacobt257@gmail.com>
2023-09-06 19:26:35 -0400

    Styled The Web Page With Borders

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,36 @@
 import {puppyList} from './data.js'
 import {useState} from 'react'
 import './App.css'
+import './index.css'
 
-
+// Define the main App component
 function App() {
+  // Initialize the 'puppies' state variable with the puppyList data
   const [puppies, setPuppies] = useState(puppyList);
+
+  // Initialize the 'featPupId' state variable as null
   const [featPupId, setFeatPupId] = useState(null);
 
+  // Find the featured puppy using its ID fro the 'puppies' array
   const featuredPup = puppies.find((pup) => pup.id === featPupId);
 
+  // Log the featured puppy to the console
   console.log(featuredPup);
 
   return (
-    <div className="App">
-      {puppies.map((puppy) => {
-        return (
-          <p onClick={()=>{console.log(setFeatPupId(puppy.id))}} key={puppy.id}>{puppy.name}</p>
+    // Render the main container for the application
+    <div className="app-container">
+      <div className="puppy-list">
+        {puppies.map((puppy) => {
+          return (
+            <p onClick={()=>{console.log(setFeatPupId(puppy.id))}} key={puppy.id}>{puppy.name}</p>
         );
       })}
+      </div>
+      
 
       {featPupId && (
-        <div>
+        <div className="featured-puppy">
           <h2>{featuredPup.name}</h2>
             <ul>
               <li>Age: {featuredPup.age}</li>
